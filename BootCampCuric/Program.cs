28b5635062546ea506c97171c521b2be807eb6ff
Michael Walker <walkerrepair255@yahoo.com>
2021-10-03 15:58:59 -0400

    Add project files.

---
+++ BootCampCuric/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BootCampCuric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gitgithub = new Topic();
+            gitgithub.Name = "Git/GitHub";
+            gitgithub.Category = "SCM";
+            gitgithub.Difficulty = 2;
+            var sql = new Topic()
+            {
+                Name = "SQL",
+                Category = "Data Management",
+                Difficulty = 1
+            };
+            var csharp = new Topic()
+            {
+                Name = "csharp",
+                Category = "Encapsalation and interaction",
+                Difficulty = 3
+            };
+            var java = new Topic()
+            {
+                Name = "Java",
+                Category = "For C#",
+                Difficulty = 2
+            };
+            gitgithub.Print();
+            sql.Print();
+            csharp.Print();
+            java.Print();
+            
+            
+        
+                
+        
+            
+        }
+    }
+}
---
+++ BootCampCuric/Topic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCampCuric
+{
+    class Topic
+    {
+        private static int NextId { get; set; } = 1;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Difficulty { get; set; }
+
+        public Topic()
+        {   //this. is not need but here just as an example. we only need this. when property and parameters are the same name.
+            this.Id = NextId++;
+            //NextId = NextId + 2;         //this line is for more than 1.
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{Id} | {Name} | {Category} | {Difficulty}");
+        }
+
+    }
+}
